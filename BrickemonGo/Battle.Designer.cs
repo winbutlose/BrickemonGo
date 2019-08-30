c6378d624b4f32da0b4fc918c242ca337eaa5145
Winbutlose <Winbutlose@gmail.com>
2019-08-29 22:36:11 -0400

    stopping point
    started new battle layout gui and started burn + paralyze affecting pokemon attacks but need debug prints to confirm effects :)

--- BrickemonGo/Battle.Designer.cs
+++ BrickemonGo/Battle.Designer.cs
@@ -31,6 +31,8 @@
             this.PictureBoxOppPoke = new System.Windows.Forms.PictureBox();
             this.PictureBoxUserPoke = new System.Windows.Forms.PictureBox();
             this.MainPanel = new System.Windows.Forms.Panel();
+            this.statuspicbox = new System.Windows.Forms.PictureBox();
+            this.oppstatuspicbox = new System.Windows.Forms.PictureBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -40,35 +42,43 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.breakinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.oppstatuspicbox = new System.Windows.Forms.PictureBox();
-            this.statuspicbox = new System.Windows.Forms.PictureBox();
+            this.pokeinfopanel = new System.Windows.Forms.Panel();
+            this.pokename = new System.Windows.Forms.Label();
+            this.pokehp = new System.Windows.Forms.Label();
+            this.opppokepanel = new System.Windows.Forms.Panel();
+            this.opppokehp = new System.Windows.Forms.Label();
+            this.opppokename = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxOppPoke)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUserPoke)).BeginInit();
             this.MainPanel.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.oppstatuspicbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.statuspicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oppstatuspicbox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.pokeinfopanel.SuspendLayout();
+            this.opppokepanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // PictureBoxOppPoke
             // 
-            this.PictureBoxOppPoke.Location = new System.Drawing.Point(650, 12);
+            this.PictureBoxOppPoke.Location = new System.Drawing.Point(698, 191);
             this.PictureBoxOppPoke.Name = "PictureBoxOppPoke";
-            this.PictureBoxOppPoke.Size = new System.Drawing.Size(250, 250);
+            this.PictureBoxOppPoke.Size = new System.Drawing.Size(150, 150);
             this.PictureBoxOppPoke.TabIndex = 0;
             this.PictureBoxOppPoke.TabStop = false;
             // 
             // PictureBoxUserPoke
             // 
-            this.PictureBoxUserPoke.Location = new System.Drawing.Point(12, 12);
+            this.PictureBoxUserPoke.Location = new System.Drawing.Point(612, 386);
             this.PictureBoxUserPoke.Name = "PictureBoxUserPoke";
-            this.PictureBoxUserPoke.Size = new System.Drawing.Size(250, 250);
+            this.PictureBoxUserPoke.Size = new System.Drawing.Size(150, 150);
             this.PictureBoxUserPoke.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PictureBoxUserPoke.TabIndex = 1;
             this.PictureBoxUserPoke.TabStop = false;
             // 
             // MainPanel
             // 
+            this.MainPanel.Controls.Add(this.opppokepanel);
+            this.MainPanel.Controls.Add(this.pokeinfopanel);
             this.MainPanel.Controls.Add(this.statuspicbox);
             this.MainPanel.Controls.Add(this.oppstatuspicbox);
             this.MainPanel.Controls.Add(this.button4);
@@ -82,12 +92,30 @@
             this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainPanel.Location = new System.Drawing.Point(0, 24);
             this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(1191, 696);
+            this.MainPanel.Size = new System.Drawing.Size(1504, 942);
             this.MainPanel.TabIndex = 2;
+            // 
+            // statuspicbox
+            // 
+            this.statuspicbox.Location = new System.Drawing.Point(556, 486);
+            this.statuspicbox.Name = "statuspicbox";
+            this.statuspicbox.Size = new System.Drawing.Size(50, 50);
+            this.statuspicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.statuspicbox.TabIndex = 11;
+            this.statuspicbox.TabStop = false;
+            // 
+            // oppstatuspicbox
+            // 
+            this.oppstatuspicbox.Location = new System.Drawing.Point(854, 291);
+            this.oppstatuspicbox.Name = "oppstatuspicbox";
+            this.oppstatuspicbox.Size = new System.Drawing.Size(50, 50);
+            this.oppstatuspicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.oppstatuspicbox.TabIndex = 10;
+            this.oppstatuspicbox.TabStop = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(588, 664);
+            this.button4.Location = new System.Drawing.Point(978, 886);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(186, 44);
             this.button4.TabIndex = 9;
@@ -97,7 +125,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(204, 664);
+            this.button2.Location = new System.Drawing.Point(594, 886);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(186, 44);
             this.button2.TabIndex = 8;
@@ -107,7 +135,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(396, 664);
+            this.button3.Location = new System.Drawing.Point(786, 886);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(186, 44);
             this.button3.TabIndex = 7;
@@ -117,7 +145,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 664);
+            this.button1.Location = new System.Drawing.Point(402, 886);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(186, 44);
             this.button1.TabIndex = 4;
@@ -129,7 +157,7 @@
             // 
             this.textboxwords.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.textboxwords.Font = new System.Drawing.Font("Dubai", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textboxwords.Location = new System.Drawing.Point(12, 388);
+            this.textboxwords.Location = new System.Drawing.Point(317, 827);
             this.textboxwords.Name = "textboxwords";
             this.textboxwords.ReadOnly = true;
             this.textboxwords.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -139,7 +167,7 @@
             // outputbox
             // 
             this.outputbox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.outputbox.Location = new System.Drawing.Point(963, 12);
+            this.outputbox.Location = new System.Drawing.Point(1271, 12);
             this.outputbox.Name = "outputbox";
             this.outputbox.ReadOnly = true;
             this.outputbox.Size = new System.Drawing.Size(221, 696);
@@ -153,7 +181,7 @@
             this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1191, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1504, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -168,27 +196,67 @@
             // breakinToolStripMenuItem
             // 
             this.breakinToolStripMenuItem.Name = "breakinToolStripMenuItem";
-            this.breakinToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.breakinToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
             this.breakinToolStripMenuItem.Text = "Break-in";
             this.breakinToolStripMenuItem.Click += new System.EventHandler(this.BreakinToolStripMenuItem_Click);
             // 
-            // oppstatuspicbox
+            // pokeinfopanel
             // 
-            this.oppstatuspicbox.Location = new System.Drawing.Point(594, 212);
-            this.oppstatuspicbox.Name = "oppstatuspicbox";
-            this.oppstatuspicbox.Size = new System.Drawing.Size(50, 50);
-            this.oppstatuspicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.oppstatuspicbox.TabIndex = 10;
-            this.oppstatuspicbox.TabStop = false;
+            this.pokeinfopanel.Controls.Add(this.pokehp);
+            this.pokeinfopanel.Controls.Add(this.pokename);
+            this.pokeinfopanel.Location = new System.Drawing.Point(419, 386);
+            this.pokeinfopanel.Name = "pokeinfopanel";
+            this.pokeinfopanel.Size = new System.Drawing.Size(137, 96);
+            this.pokeinfopanel.TabIndex = 12;
             // 
-            // statuspicbox
+            // pokename
             // 
-            this.statuspicbox.Location = new System.Drawing.Point(268, 212);
-            this.statuspicbox.Name = "statuspicbox";
-            this.statuspicbox.Size = new System.Drawing.Size(50, 50);
-            this.statuspicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.statuspicbox.TabIndex = 11;
-            this.statuspicbox.TabStop = false;
+            this.pokename.AutoSize = true;
+            this.pokename.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pokename.Location = new System.Drawing.Point(17, 10);
+            this.pokename.Name = "pokename";
+            this.pokename.Size = new System.Drawing.Size(49, 18);
+            this.pokename.TabIndex = 0;
+            this.pokename.Text = "poke1";
+            // 
+            // pokehp
+            // 
+            this.pokehp.AutoSize = true;
+            this.pokehp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pokehp.Location = new System.Drawing.Point(17, 39);
+            this.pokehp.Name = "pokehp";
+            this.pokehp.Size = new System.Drawing.Size(52, 18);
+            this.pokehp.TabIndex = 1;
+            this.pokehp.Text = "hp: 0/0";
+            // 
+            // opppokepanel
+            // 
+            this.opppokepanel.Controls.Add(this.opppokehp);
+            this.opppokepanel.Controls.Add(this.opppokename);
+            this.opppokepanel.Location = new System.Drawing.Point(910, 191);
+            this.opppokepanel.Name = "opppokepanel";
+            this.opppokepanel.Size = new System.Drawing.Size(140, 96);
+            this.opppokepanel.TabIndex = 13;
+            // 
+            // opppokehp
+            // 
+            this.opppokehp.AutoSize = true;
+            this.opppokehp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opppokehp.Location = new System.Drawing.Point(17, 39);
+            this.opppokehp.Name = "opppokehp";
+            this.opppokehp.Size = new System.Drawing.Size(52, 18);
+            this.opppokehp.TabIndex = 1;
+            this.opppokehp.Text = "hp: 0/0";
+            // 
+            // opppokename
+            // 
+            this.opppokename.AutoSize = true;
+            this.opppokename.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opppokename.Location = new System.Drawing.Point(17, 10);
+            this.opppokename.Name = "opppokename";
+            this.opppokename.Size = new System.Drawing.Size(49, 18);
+            this.opppokename.TabIndex = 0;
+            this.opppokename.Text = "poke2";
             // 
             // Battle
             // 
@@ -196,7 +264,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(1191, 720);
+            this.ClientSize = new System.Drawing.Size(1504, 966);
             this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -206,10 +274,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUserPoke)).EndInit();
             this.MainPanel.ResumeLayout(false);
             this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.statuspicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oppstatuspicbox)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.oppstatuspicbox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.statuspicbox)).EndInit();
+            this.pokeinfopanel.ResumeLayout(false);
+            this.pokeinfopanel.PerformLayout();
+            this.opppokepanel.ResumeLayout(false);
+            this.opppokepanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -231,5 +303,11 @@
         private System.Windows.Forms.ToolStripMenuItem breakinToolStripMenuItem;
         private System.Windows.Forms.PictureBox statuspicbox;
         private System.Windows.Forms.PictureBox oppstatuspicbox;
+        private System.Windows.Forms.Panel pokeinfopanel;
+        private System.Windows.Forms.Label pokehp;
+        private System.Windows.Forms.Label pokename;
+        private System.Windows.Forms.Panel opppokepanel;
+        private System.Windows.Forms.Label opppokehp;
+        private System.Windows.Forms.Label opppokename;
     }
 }
